a48e452ee785f004eb6405a2d178d7e792356bef
skkeyan <skkeyan@gmail.com>
2014-05-31 20:21:28 +0000

    8080 port change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World for the first website!')
 })
 
 app.listen(app.get('port'), function() {
